28da28f39814352d3099f8ecbb8f2c386758cfe4
Hicka <cc@hicka.world>
2021-12-30 19:55:28 +0100

    feat(*): add main file and move lib to subdir

---
+++ src/main.ts
@@ -0,0 +1,4 @@
+export { default as Interval } from './lib/Interval';
+export { default as IntervalParams } from './lib/IntervalParams';
+export { default as sleep } from './lib/sleep';
+export { default as Timeout } from './lib/Timeout';
